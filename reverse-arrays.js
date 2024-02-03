7b1d86e8fdca01c3ce9cb8b0806310064311d1ee
Gavin Clark <gavinclark217@gmail.com>
2024-02-02 22:13:42 -0800

    finish reverse arrays

--- reverse-arrays.js
+++ reverse-arrays.js
@@ -1,9 +1,23 @@
 function reverseArray(arr) {
-  // Add code.
+  len = arr.length;
+  for (let i = len - 1; i >= 0; i--) {
+    arr.push(arr[i]);
+  }
+  for (let j = 0; j < len; j++) {
+    arr.shift();
+  }
+  return arr;
 }
 
 function reverseArrayInPlace(arr) {
-  // Add code.
+  let temp = 0;
+  let len = arr.length - 1;
+  for ( let i = 0; i < Math.floor(arr.length / 2); i++) {
+    temp = arr[i];
+    arr[i] = arr[len - i];
+    arr[len - i] = temp;
+  }
+  return arr;
 }
 
 
